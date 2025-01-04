26a8484771cfd4d671a1667e7943f298b073ad36
Edison Santiago <edisonluizsm@gmail.com>
2025-01-04 16:22:31 -0300

    Create fixed menu on desktop

--- components/content_dir.vue
+++ components/content_dir.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="pl-4">
     <div v-for="content in contents" :key="content._path">
-      <NuxtLink class="text-orange-400" :to="content._path">
+      <span v-if="isCurrentContent(content)" class="font-bold underline text-lg text-orange-400">{{ content.title }}</span>
+      <NuxtLink v-else class="hover:underline font-bold text-lg" :to="content._path">
         {{ content.title }}<br />
       </NuxtLink>
     </div>
     <div v-for="directory in directories" :key="directory.path">
-      {{ directory.path }}
+      <span class="text-gray-600">{{ directory.path }}</span>
       <ContentDir :dir="directory" />
     </div>
   </div>
@@ -16,10 +17,16 @@
 import type { ParsedContent } from '@nuxt/content';
 import type { ContentDir } from '~/types/content_dir';
 
+const route = useRoute()
+
 const props = defineProps<{
   dir: ContentDir
 }>();
 
 const directories = computed(() => props.dir.children.flatMap((c) => "path" in c ? <ContentDir> c : []))
-const contents = computed(() => props.dir.children.flatMap((c) => "_path" in c ? <ParsedContent> c : [])) 
+const contents = computed(() => props.dir.children.flatMap((c) => "_path" in c ? <ParsedContent> c : []))
+
+function isCurrentContent(content: ParsedContent) {
+  return content._path === route.path;
+}
 </script>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,15 @@
 <template>
-  <main>
-    <ContentDoc class="prose" />
-  </main>
+  <div class="lg:flex h-full mb-16 lg:mb-0">
+    <!-- FIXED DESKTOP MENU -->
+    <!-- SHOWS FROM LG AND ABOVE -->
+    <div class="flex-shrink-0 lg:static lg:w-60 xl:w-72 hidden lg:block border-zero-orange border-opacity-50 border-l-0 border-t-0 border-r border-b-0">
+      <TableOfContents />
+    </div>
+    <div>
+      <main>
+        <ContentDoc class="prose" />
+      </main>
+    </div>
+  </div>
+ 
 </template>
