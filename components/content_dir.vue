feade43808992ed77829accc955180eb9c36e74d
Edison Santiago <edisonluizsm@gmail.com>
2025-01-05 08:51:45 -0300

    Fix index links

--- components/content_dir.vue
+++ components/content_dir.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="pl-4">
+  <div class="pl-4 pr-2">
     <div v-for="content in contents" :key="content._path">
-      <!-- <span v-if="isCurrentContent(content)" class="font-bold underline text-lg text-orange-400">{{ content.title }}</span> -->
-      <NuxtLink class="hover:underline font-bold text-lg" exact-active-class="text-2xl text-orange-400" :to="content._path">
-        {{ content.title }}<br />
-      </NuxtLink>
+      <span v-if="isCurrentContent(content)" class="font-bold underline text-base text-orange-400">
+        {{ content.title }}
+      </span>
+      <a v-else :href="content._path" class="hover:underline font-bold text-base">
+        {{ content.title }}
+      </a>
     </div>
     <div v-for="directory in directories" :key="directory.path">
-      <span class="text-gray-600">{{ directory.path }}</span>
+      <span class="block my-2 text-gray-500 capitalize text-2xl">{{ directory.path }}</span>
       <ContentDir :dir="directory" />
     </div>
   </div>
@@ -25,4 +27,8 @@ const props = defineProps<{
 
 const directories = computed(() => props.dir.children.flatMap((c) => "path" in c ? <ContentDir> c : []))
 const contents = computed(() => props.dir.children.flatMap((c) => "_path" in c ? <ParsedContent> c : []))
+
+function isCurrentContent(content: ParsedContent) {
+  return content._path === route.path
+}
 </script>
